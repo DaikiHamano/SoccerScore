7ff492b3ac45efaed77f8a6aed309c0ac44122da
Daiki Hamano <200438475@student.georgianc.on.ca>
2021-04-02 23:57:20 +0900

    second

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class Main extends Application{
@@ -15,6 +16,8 @@ public class Main extends Application{
         Parent root = FXMLLoader.load(getClass().getResource("views/view.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UTILITY);
+        root.setStyle("-fx-background-color: white ;");
         stage.show();
     }
 }
