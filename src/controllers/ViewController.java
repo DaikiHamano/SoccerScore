2c3147817ac9cd94734ef1a1ff72bf114767f329
Daiki Hamano <200438475@student.georgianc.on.ca>
2021-04-04 23:13:25 +0900

    Fifth almost done

--- src/controllers/ViewController.java
+++ src/controllers/ViewController.java
@@ -3,10 +3,8 @@ package controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import models.Data;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,6 +18,9 @@ public class ViewController implements Initializable {
 
     @FXML
     private TextField goal;
+
+    @FXML
+    private TextField pk;
 
     @FXML
     private TextField assist;
@@ -67,7 +68,13 @@ public class ViewController implements Initializable {
     private TextField accLb;
 
     @FXML
-    private TextField redCard;
+    private ToggleGroup red;
+
+    @FXML
+    private RadioButton yes;
+
+    @FXML
+    private RadioButton no;
 
     @FXML
     private TextField givePk;
@@ -81,50 +88,64 @@ public class ViewController implements Initializable {
     @FXML
     private Label errorMessage;
 
+    @FXML
+    private Label score;
+
+    //to reset the message first
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         errorMessage.setText("");
+        score.setText("");
     }
 
+    //to check the blank and if the word is correct or not
     @FXML
     void ok(ActionEvent event) {
-        if(arsenalScore.getText().isBlank()){
+        //to check the blank
+        if(arsenalScore.getText().isBlank()||opponentScore.getText().isBlank()||goal.getText().isBlank()||
+                assist.getText().isBlank()||shots.getText().isBlank()||keyPasses.getText().isBlank()||
+                aerialsWon.getText().isBlank()||dribbles.getText().isBlank()||disp.getText().isBlank()||
+                unsTouches.getText().isBlank()||totalTackles.getText().isBlank()||interceptions.getText().isBlank()||
+                clearances.getText().isBlank()||blockedShots.getText().isBlank()||pa.getText().isBlank()||
+                crosses.getText().isBlank()||accCrosses.getText().isBlank()||accLb.getText().isBlank()||
+                givePk.getText().isBlank()||givenPk.getText().isBlank()||pk.getText().isBlank()){
             errorMessage.setText("Fill in the blanks");
+        //to check the position name
         }else if(position.getText().equals("FW")||position.getText().equals("AM")
-                ||position.getText().equals("M") ||position.getText().equals("D")){
+                ||position.getText().equals("M")||position.getText().equals("D")){
             errorMessage.setText("");
+        //to check the score
+        }else if(Integer.parseInt(arsenalScore.getText()) < Integer.parseInt(goal.getText()) +
+                Integer.parseInt(assist.getText()) + Integer.parseInt(pk.getText())){
+            errorMessage.setText("Check the goal and assist");
         }else{
             errorMessage.setText("Put a proper position");
         }
 
-
+        //to put the data to the object
         Data data = new Data(Integer.parseInt(arsenalScore.getText()),Integer.parseInt(opponentScore.getText()),
-                Integer.parseInt(goal.getText()),Integer.parseInt(assist.getText()),
+                Integer.parseInt(goal.getText()),Integer.parseInt(pk.getText()),Integer.parseInt(assist.getText()),
                 Integer.parseInt(shots.getText()),Integer.parseInt(keyPasses.getText()),
                 Integer.parseInt(aerialsWon.getText()),Integer.parseInt(dribbles.getText()),
                 Integer.parseInt(disp.getText()),Integer.parseInt(unsTouches.getText()),
                 Integer.parseInt(totalTackles.getText()),Integer.parseInt(interceptions.getText()),
                 Integer.parseInt(clearances.getText()),Integer.parseInt(blockedShots.getText()),
-                Integer.parseInt(pa.getText()),Integer.parseInt(crosses.getText()),
+                Double.parseDouble(pa.getText()),Integer.parseInt(crosses.getText()),
                 Integer.parseInt(accCrosses.getText()),Integer.parseInt(accLb.getText()),
-                Integer.parseInt(redCard.getText()),Integer.parseInt(givenPk.getText()),
-                Integer.parseInt(givenPk.getText()),position.getText());
+                Integer.parseInt(givenPk.getText()), Integer.parseInt(givenPk.getText()),position.getText());
+
+
+        score.setText(String.valueOf(data.getTotal()));
     }
 
+    //for deleting
     @FXML
     void reset(ActionEvent event) {
-        arsenalScore.clear();opponentScore.clear();goal.clear();
+        arsenalScore.clear();opponentScore.clear();goal.clear();pk.clear();
         assist.clear();shots.clear();keyPasses.clear();aerialsWon.clear();dribbles.clear();disp.clear();
         unsTouches.clear();totalTackles.clear();interceptions.clear();
         clearances.clear();blockedShots.clear();pa.clear();crosses.clear();
-        accCrosses.clear();accLb.clear();redCard.clear();givePk.clear();
+        accCrosses.clear();accLb.clear();givePk.clear();
         givenPk.clear();position.clear();
     }
-
-
-
-
-//if(position.equals("FW")||position.equals("AM")||position.equals("M")||position.equals("D")){
-//        this.position = position;
-//    }
 }
--- src/models/Data.java
+++ src/models/Data.java
@@ -1,214 +1,269 @@
 package models;
 
+import javafx.scene.control.Toggle;
+
 public class Data {
 
-       private int arsenalScore,opponentScore,goal,assist,shots,keyPasses,aerialsWon,dribbles,disp,
-                 unsTouches,totalTackles,interceptions,clearances,blockedShots,pa,crosses,
-                 accCrosses,accLb,redCard,givePk,givenPk;
+       private int arsenalScore,opponentScore,goal,pk,assist,shots,keyPasses,aerialsWon,dribbles,disp,
+                 unsTouches,totalTackles,interceptions,clearances,blockedShots,crosses,
+                 accCrosses,accLb,givePk,givenPk;
+       private double pa;
        private String position;
+       //to evaluate the score
+       private double total;
 
+       public Data(int arsenalScore, int opponentScore, int goal, int pk,int assist, int shots, int keyPasses,
+                   int aerialsWon, int dribbles, int disp, int unsTouches, int totalTackles, int interceptions,
+                   int clearances, int blockedShots, double pa, int crosses, int accCrosses, int accLb,
+                   int givePk, int givenPk, String position) {
+                          setArsenalScore(arsenalScore);
+                          setOpponentScore(opponentScore);
+                          setGoal(goal);
+                          setPk(pk);
+                          setAssist(assist);
+                          setShots(shots);
+                          setKeyPasses(keyPasses);
+                          setAerialsWon(aerialsWon);
+                          setDribbles(dribbles);
+                          setDisp(disp);
+                          setUnsTouches(unsTouches);
+                          setTotalTackles(totalTackles);
+                          setInterceptions(interceptions);
+                          setClearances(clearances);
+                          setBlockedShots(blockedShots);
+                          setPa(pa);
+                          setCrosses(crosses);
+                          setAccCrosses(accCrosses);
+                          setAccLb(accLb);
+                          setGivePk(givePk);
+                          setGivenPk(givenPk);
+                          setPosition(position);
+       }
 
- public Data(int arsenalScore, int opponentScore, int goal, int assist, int shots, int keyPasses,
-             int aerialsWon,int dribbles, int disp, int unsTouches, int totalTackles, int interceptions,
-             int clearances, int blockedShots, int pa, int crosses, int accCrosses, int accLb,
-             int redCard, int givePk, int givenPk, String position) {
-                    setArsenalScore(arsenalScore);
-                    setOpponentScore(opponentScore);
-                    setGoal(goal);
-                    setAssist(assist);
-                    setShots(shots);
-                    setKeyPasses(keyPasses);
-                    setAerialsWon(aerialsWon);
-                    setDribbles(dribbles);
-                    setDisp(disp);
-                    setUnsTouches(unsTouches);
-                    setTotalTackles(totalTackles);
-                    setInterceptions(interceptions);
-                    setClearances(clearances);
-                    setBlockedShots(blockedShots);
-                    setPa(pa);
-                    setCrosses(crosses);
-                    setAccCrosses(accCrosses);
-                    setAccLb(accLb);
-                    setRedCard(redCard);
-                    setGivePk(givePk);
-                    setGivenPk(givenPk);
-                    setPosition(position);
- }
+       public int getArsenalScore() {
+              return arsenalScore;
+       }
 
- public int getArsenalScore() {
-  return arsenalScore;
- }
+       public void setArsenalScore(int arsenalScore) {
+          this.arsenalScore = arsenalScore;
+       }
 
- public void setArsenalScore(int arsenalScore) {
-  this.arsenalScore = arsenalScore;
- }
+       public int getOpponentScore() {
+          return opponentScore;
+       }
 
- public int getOpponentScore() {
-  return opponentScore;
- }
+       public void setOpponentScore(int opponentScore) {
+          this.opponentScore = opponentScore;
+       }
 
- public void setOpponentScore(int opponentScore) {
-  this.opponentScore = opponentScore;
- }
+       public int getGoal() {
+          return goal;
+       }
 
- public int getGoal() {
-  return goal;
- }
+       public void setGoal(int goal) {
+          this.goal = goal;
+       }
 
- public void setGoal(int goal) {
-  this.goal = goal;
- }
+       public int getPk() {
+            return pk;
+       }
 
- public int getAssist() {
-  return assist;
- }
+       public void setPk(int pk) {
+            this.pk = pk;
+       }
 
- public void setAssist(int assist) {
-  this.assist = assist;
- }
+       public int getAssist() {
+          return assist;
+       }
 
- public int getShots() {
-  return shots;
- }
+       public void setAssist(int assist) {
+          this.assist = assist;
+       }
 
- public void setShots(int shots) {
-  this.shots = shots;
- }
+       public int getShots() {
+          return shots;
+       }
 
- public int getKeyPasses() {
-  return keyPasses;
- }
+       public void setShots(int shots) {
+          this.shots = shots;
+       }
 
- public void setKeyPasses(int keyPasses) {
-  this.keyPasses = keyPasses;
- }
+       public int getKeyPasses() {
+          return keyPasses;
+       }
 
- public int getAerialsWon() {
-  return aerialsWon;
- }
+       public void setKeyPasses(int keyPasses) {
+          this.keyPasses = keyPasses;
+       }
 
- public void setAerialsWon(int aerialsWon) {
-  this.aerialsWon = aerialsWon;
- }
+       public int getAerialsWon() {
+          return aerialsWon;
+       }
 
- public int getDribbles() {
-  return dribbles;
- }
+       public void setAerialsWon(int aerialsWon) {
+          this.aerialsWon = aerialsWon;
+       }
 
- public void setDribbles(int dribbles) {
-  this.dribbles = dribbles;
- }
+       public int getDribbles() {
+          return dribbles;
+       }
 
- public int getDisp() {
-  return disp;
- }
+       public void setDribbles(int dribbles) {
+          this.dribbles = dribbles;
+       }
 
- public void setDisp(int disp) {
-  this.disp = disp;
- }
+       public int getDisp() {
+          return disp;
+       }
 
- public int getUnsTouches() {
-  return unsTouches;
- }
+       public void setDisp(int disp) {
+          this.disp = disp;
+       }
 
- public void setUnsTouches(int unsTouches) {
-  this.unsTouches = unsTouches;
- }
+       public int getUnsTouches() {
+          return unsTouches;
+       }
 
- public int getTotalTackles() {
-  return totalTackles;
- }
+       public void setUnsTouches(int unsTouches) {
+          this.unsTouches = unsTouches;
+       }
 
- public void setTotalTackles(int totalTackles) {
-  this.totalTackles = totalTackles;
- }
+       public int getTotalTackles() {
+          return totalTackles;
+       }
 
- public int getInterceptions() {
-  return interceptions;
- }
+       public void setTotalTackles(int totalTackles) {
+          this.totalTackles = totalTackles;
+       }
 
- public void setInterceptions(int interceptions) {
-  this.interceptions = interceptions;
- }
+       public int getInterceptions() {
+          return interceptions;
+       }
 
- public int getClearances() {
-  return clearances;
- }
+       public void setInterceptions(int interceptions) {
+          this.interceptions = interceptions;
+       }
 
- public void setClearances(int clearances) {
-  this.clearances = clearances;
- }
+       public int getClearances() {
+          return clearances;
+       }
 
- public int getBlockedShots() {
-  return blockedShots;
- }
+       public void setClearances(int clearances) {
+          this.clearances = clearances;
+       }
 
- public void setBlockedShots(int blockedShots) {
-  this.blockedShots = blockedShots;
- }
+       public int getBlockedShots() {
+          return blockedShots;
+       }
 
- public int getPa() {
-  return pa;
- }
+       public void setBlockedShots(int blockedShots) {
+          this.blockedShots = blockedShots;
+       }
 
- public void setPa(int pa) {
-  this.pa = pa;
- }
+       public double getPa() {
+          return pa;
+       }
 
- public int getCrosses() {
-  return crosses;
- }
+       public void setPa(double pa) {
+          this.pa = pa;
+       }
 
- public void setCrosses(int crosses) {
-  this.crosses = crosses;
- }
+       public int getCrosses() {
+          return crosses;
+       }
 
- public int getAccCrosses() {
-  return accCrosses;
- }
+       public void setCrosses(int crosses) {
+          this.crosses = crosses;
+       }
 
- public void setAccCrosses(int accCrosses) {
-  this.accCrosses = accCrosses;
- }
+       public int getAccCrosses() {
+          return accCrosses;
+       }
 
- public int getAccLb() {
-  return accLb;
- }
+       public void setAccCrosses(int accCrosses) {
+          this.accCrosses = accCrosses;
+       }
 
- public void setAccLb(int accLb) {
-  this.accLb = accLb;
- }
+       public int getAccLb() {
+          return accLb;
+       }
 
- public int getRedCard() {
-  return redCard;
- }
+       public void setAccLb(int accLb) {
+          this.accLb = accLb;
+       }
 
- public void setRedCard(int redCard) {
-  this.redCard = redCard;
- }
+       public int getGivePk() {
+          return givePk;
+       }
 
- public int getGivePk() {
-  return givePk;
- }
+       public void setGivePk(int givePk) {
+          this.givePk = givePk;
+       }
 
- public void setGivePk(int givePk) {
-  this.givePk = givePk;
- }
+       public int getGivenPk() {
+          return givenPk;
+       }
 
- public int getGivenPk() {
-  return givenPk;
- }
+       public void setGivenPk(int givenPk) {
+          this.givenPk = givenPk;
+       }
 
- public void setGivenPk(int givenPk) {
-  this.givenPk = givenPk;
- }
+       public String getPosition() {
+          return position;
+       }
 
- public String getPosition() {
-  return position;
- }
+       public void setPosition(String position) {
+          this.position = position;
+       }
 
- public void setPosition(String position) {
-  this.position = position;
- }
+       //this is a method to calculate the total
+       public double getTotal(){
+           total = total + goal + pk * 0.5 + assist * 0.5 + keyPasses * 0.25
+                         + aerialsWon * 0.1 + dribbles * 0.1 + clearances * 0.1
+                         + accLb * 0.1 + givePk * -1 + givenPk + crosses * 0.05
+                         + accCrosses * 0.15;
+
+          if(position.equals("FW")||position.equals("AM")){
+             if(arsenalScore == 0){
+                 total = total - 0.5;
+             }else{
+                 total = total + arsenalScore * 0.1;
+             }
+          }else{
+             if(opponentScore == 0){
+                 total = total + 1;
+             }else{
+                 total = total - opponentScore * 0.1;
+             }
+          }
+
+          if(shots == 0){
+             if(position.equals("FW")){
+                 total = total - 1;
+             }
+          }else{
+             total = total + shots * 0.1;
+          }
+
+          if(position.equals("D")){
+              total = total + disp * -0.2 + unsTouches * -0.2;
+          }else{
+              total = total + disp * -0.1 + unsTouches * -0.1;
+          }
+
+          if(position.equals("FW")){
+              total = total + totalTackles * 0.25 + interceptions * 0.25 + blockedShots * 0.3;
+          }else{
+              total = total + totalTackles * 0.15 + interceptions * 0.15 + blockedShots * 0.2;
+          }
+
+          if(pa >= 90){
+              total = total + 0.1;
+          }
+          if(pa <= 70){
+              total = total - 0.1;
+          }
+
+          return total;
+       }
 }
